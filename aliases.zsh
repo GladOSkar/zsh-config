3787c4f0f17397f2a80b0f104f6463cff0640660
Oskar <oskar@winkels.berlin>
2019-10-08 19:51:41 +0200

    fix 2

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,5 @@
-alias l='which exa > /dev/null && exa -1F --group-directories-first || ls -1'
-alias ls='which exa > /dev/null && exa -laF --group-directories-first || ls -la'
+alias l='which exa > /dev/null && exa -1F --group-directories-first || command ls -1'
+alias ls='which exa > /dev/null && exa -laF --group-directories-first || command ls -la'
 alias j='jump'
 alias dirt='watch grep -e "Dirty" -e "Writeback" /proc/meminfo'
 alias codelines="sed '/^\s*$/d' | wc -l"
@@ -8,10 +8,10 @@ alias f='find . -iname'
 alias c='clear'
 
 function sv() {
-	which nvim > /dev/null && sudo nvim $@ || (which vim > /dev/null && sudo vim $@ || sudo nano $@)
+    which nvim > /dev/null && sudo nvim $@ || (which vim > /dev/null && sudo vim $@ || sudo nano $@)
 }
 function v() {
-	which nvim > /dev/null && nvim $@ || (which vim > /dev/null && vim $@ || nano $@)
+    which nvim > /dev/null && nvim $@ || (which vim > /dev/null && vim $@ || nano $@)
 }
 
 alias ...='../..'
@@ -28,6 +28,6 @@ mkd() {
 
 cd() {
     builtin cd $@
-    which exa > /dev/null && exa -F --group-directories-first || ls
+    which exa > /dev/null && exa -F --group-directories-first || command ls
 }
 
